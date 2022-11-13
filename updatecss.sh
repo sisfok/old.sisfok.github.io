b325375fe2ff1e4cec21641dff8fb8567c969a17
Deo Valiandro. M <valiandrod@gmail.com>
2022-11-13 21:53:07 +0800

    remove nav width
    Signed-off-by: Deo Valiandro. M <valiandrod@gmail.com>

--- updatecss.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cd /home/deo/labs/classless-css
-git add .
-git commit -s -S -m "update css"
-git push origin main
-cd /home/deo/labs/sisfok.github.io
-git submodule update --recursive --remote
-jekyll serve
